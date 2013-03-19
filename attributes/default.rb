b04e077d7f01ddf84e0a91eb00c4a0a7d56be65e
Gerhard Lazu <gerhard@lazu.co.uk>
2013-03-19 17:59:58 +0000

    Update rbenv & ruby to latest

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 set[:rbenv][:dir]             = "/usr/local/rbenv"
 set[:rbenv][:repository]      = "git://github.com/sstephenson/rbenv.git"
-default[:rbenv][:version]     = "v0.3.0"
+default[:rbenv][:version]     = "v0.4.0"
 default[:rbenv][:action]      = "install" # anything else will remove rbenv from the system
 default[:rbenv][:global_ruby] = false
 default[:rbenv][:global_gems] = %w[bundler pry foreman]
@@ -9,6 +9,6 @@ set[:ruby_build][:dir]         = "/usr/local/ruby-build"
 set[:ruby_build][:install_dir] = "/usr/local/rbenv/versions"
 set[:ruby_build][:keep_dir]    = "/usr/local/src/ruby-build"
 set[:ruby_build][:repository]  = "git://github.com/sstephenson/ruby-build.git"
-default[:ruby_build][:version] = "v20120815"
+default[:ruby_build][:version] = "v20130227"
 default[:ruby_build][:action]  = "install" # anything else will remove ruby-build from the system
 default[:ruby_build][:rubies]  = []
