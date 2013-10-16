cf2fe133d66cb3ac9944d45687fff26c45e5d773
Gerhard Lazu <gerhard@lazu.co.uk>
2013-10-16 11:11:32 +0100

    Using http for github (think firewalled envs)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 set[:rbenv][:dir]              = "/usr/local/rbenv"
-set[:rbenv][:repository]       = "git://github.com/sstephenson/rbenv.git"
+set[:rbenv][:repository]       = "http://github.com/sstephenson/rbenv.git"
 default[:rbenv][:version]      = "v0.4.0"
 default[:rbenv][:action]       = "install" # anything else will remove rbenv from the system
 default[:rbenv][:global_ruby]  = false
@@ -8,7 +8,7 @@ default[:rbenv][:global_gems]  = %w[bundler pry foreman]
 set[:ruby_build][:dir]         = "/usr/local/ruby-build"
 set[:ruby_build][:install_dir] = "/usr/local/rbenv/versions"
 set[:ruby_build][:keep_dir]    = "/usr/local/src/ruby-build"
-set[:ruby_build][:repository]  = "git://github.com/sstephenson/ruby-build.git"
+set[:ruby_build][:repository]  = "http://github.com/sstephenson/ruby-build.git"
 default[:ruby_build][:version] = "v20130806"
 default[:ruby_build][:action]  = "install" # anything else will remove ruby-build from the system
 default[:ruby_build][:rubies]  = []
